f297c5fee4e9e9cb2d65d4b7cf2a6e89ebd5cb73
songlinhe <songlinhe@hotmail.com>
2020-09-27 09:01:02 +0800

    作业

---
+++ Week_03/226.翻转二叉树--迭代.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=cpp
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (76.43%)
+ * Likes:    638
+ * Dislikes: 0
+ * Total Accepted:    147.6K
+ * Total Submissions: 190.9K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 翻转一棵二叉树。
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ * 
+ * 输出：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ * 
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // terminator 
+        if (root == nullptr) return root;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front(); q.pop();
+            
+            if (node == nullptr) continue;
+            if (node->left != nullptr || node->right != nullptr) {
+                q.push(node->left);
+                q.push(node->right);
+            }
+            swap(node->left, node->right);
+        }
+
+        return root;
+    }
+};
+// @lc code=end
+
